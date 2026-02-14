bb42b2f6a4a016bf55c368befb8d146307aeac82
Md. Shihabul Islam <166291764+Shihab23033@users.noreply.github.com>
2026-02-14 14:33:26 +0600

    up

---
+++ studentLogIn/src/main/java/com/example/studentLogIn/StudentLogInApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentLogIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLogInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLogInApplication.class, args);
+	}
+
+}
---
+++ studentLogIn/src/test/java/com/example/studentLogIn/StudentLogInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentLogIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLogInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
